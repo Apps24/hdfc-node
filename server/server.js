7309a4da3141c57337deadd241409194b86ad653
Apurv Patole <apurvpatole2@gmail.com>
2023-03-30 14:46:23 +0530

    ap-new

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ app.start = function() {
   // start the web server
   return app.listen(function() {
     app.emit('started');
+    console.log(app.get('url'));
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
